f749192fc38e791360927edd139e9eb23518c09a
George Choustoulakis <george@choustoulakis.com>
2025-12-05 23:48:53 +0100

    Fix

--- drivers/connected_controller/driver.ts
+++ drivers/connected_controller/driver.ts
@@ -34,7 +34,6 @@ export class ConnectedControllerDriver extends Homey.Driver {
         jwt: '',
       },
     }));
-    randomUUID();
     devicesFound.push({
       name: 'Manual Configuration',
       data: {
