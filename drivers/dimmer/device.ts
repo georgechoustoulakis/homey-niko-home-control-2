948e7fadc2c2ff0d9a626eba2a2d5bd0efac78ea
George Choustoulakis <george@choustoulakis.com>
2025-12-05 23:48:53 +0100

    Add dimmer

---
+++ drivers/dimmer/device.ts
@@ -0,0 +1,40 @@
+import { NikoDevice } from '../../src/NikoDevice';
+import { DeviceStore } from '../../src/GenericDevicePairingData';
+
+class NikoDimmer extends NikoDevice {
+  async onInit(): Promise<void> {
+    this.device = (this.getStore() as DeviceStore).device;
+    console.log('Initializing Niko Dimmer device:', this.device);
+    console.log('Initializing Niko Dimmer device:', this.device.Properties);
+    await super.onInit();
+    this.registerCapabilityListener('onoff', this.onValueChange);
+    this.registerCapabilityListener('dim', this.onDimValueChange);
+    await this.updateStatus();
+  }
+
+  private onValueChange = async (value: boolean) => {
+    this.setNikoDeviceProps([{ Status: value ? 'On' : 'Off' }]);
+  };
+
+  private onDimValueChange = async (value: number) => {
+    this.setNikoDeviceProps([{ Brightness: String((value * 100).toFixed(0)) }]);
+  };
+
+  async updateStatus(): Promise<void> {
+    const statusProp = this.device.Properties.find((prop) =>
+      Object.prototype.hasOwnProperty.call(prop, 'Status'),
+    );
+    const brightnessProp = this.device.Properties.find((prop) =>
+      Object.prototype.hasOwnProperty.call(prop, 'Brightness'),
+    );
+    if (!statusProp || !brightnessProp) {
+      console.warn(`Warning: Device "${this.device.Name}" has no 'Status' property.`);
+      return this.setUnavailable('Device is misconfigured, please re-create it.');
+    }
+    await this.setAvailable();
+    await this.setCapabilityValue('onoff', statusProp.Status === 'On');
+    await this.setCapabilityValue('dim', Number(brightnessProp.Brightness) / 100);
+  }
+}
+
+module.exports = NikoDimmer;
