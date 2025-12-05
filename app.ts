7730b53ee16fd9f95cce48fa9a9c30fdb5c6ece2
George Choustoulakis <george@choustoulakis.com>
2025-12-05 23:48:51 +0100

    Rename

--- app.ts
+++ app.ts
@@ -2,11 +2,11 @@
 
 import Homey from 'homey';
 
-module.exports = class MyApp extends Homey.App {
+module.exports = class NikoHomeControlApp extends Homey.App {
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
-    this.log('MyApp has been initialized');
+    this.log('NikoHomeControlApp has been initialized');
   }
 };
