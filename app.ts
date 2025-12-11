301b320cfc0798765de76ce53a15dabae091d6ea
George Choustoulakis <george@choustoulakis.com>
2025-12-11 21:13:47 +0100

    Tweak

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import Homey from 'homey';
 export class NikoHomeControlApp extends Homey.App {
   async onInit() {
     // TODO dont register the same event name for every device
-    this.homey.setMaxListeners(200);
+    this.homey.setMaxListeners(Infinity);
   }
 }
 
