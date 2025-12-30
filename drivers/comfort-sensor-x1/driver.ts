f20bf9cdb8f27d89328541d97c09be9a63b2e0e8
George Choustoulakis <george@choustoulakis.com>
2025-12-30 21:46:09 +0100

    Tweak name

--- drivers/comfort-sensor-x1/driver.ts
+++ drivers/comfort-sensor-x1/driver.ts
@@ -1,10 +1,10 @@
 import { NikoDriver } from '../../src/NikoDriver';
 import { DevicePairingData } from '../../src/GenericDevicePairingData';
 
-class NikoComfortSensorDriver extends NikoDriver {
+class NikoSingleComfortSensorDriver extends NikoDriver {
   async onPairListDevices(): Promise<DevicePairingData[]> {
     return this.getNikoByTypeAndModel('multisensor', ['thermoswitchx1', 'thermoswitchx1feedback']);
   }
 }
 
-module.exports = NikoComfortSensorDriver;
+module.exports = NikoSingleComfortSensorDriver;
