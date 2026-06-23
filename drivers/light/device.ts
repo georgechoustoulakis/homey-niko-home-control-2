06b1746713348fc2cc5a877212faac375346539a
George Choustoulakis <george@choustoulakis.com>
2026-06-23 21:52:00 +0200

    Merge files

--- drivers/light/device.ts
+++ drivers/light/device.ts
@@ -1,3 +1,25 @@
-import { OnOffDevice } from '../../src/OnOffDevice';
+import { NikoDeviceKey } from '../connected-controller/NikoTypes';
+import { NikoDevice } from '../../src/NikoDevice';
 
-module.exports = OnOffDevice;
+export class NikoLightDevice extends NikoDevice<NikoDeviceKey.LIGHT> {
+  async onInit(): Promise<void> {
+    await super.onInit();
+    this.registerCapabilityListener('onoff', this.onValueChange);
+    await this.updateStatus();
+  }
+
+  private onValueChange = async (value: boolean) => {
+    this.setNikoDeviceProps([{ Status: value ? 'On' : 'Off' }]);
+  };
+
+  async updateStatus(): Promise<void> {
+    const status = this.getProperty('Status');
+    if (status === undefined) {
+      return this.setUnavailable('Device is misconfigured, please re-create it.');
+    }
+    await this.setAvailable();
+    await this.setCapabilityValue('onoff', status === 'On');
+  }
+}
+
+module.exports = NikoLightDevice;
